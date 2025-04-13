0089f9fe965ec633a0e3ceca6fc246f7a85cedb1
Jan_Schejbal <jan.schejbal@tul.cz>
2025-04-13 08:32:53 +0200

    detekce kolízí se stěnami

--- PG2Projekt/app.hpp
+++ PG2Projekt/app.hpp
@@ -87,4 +87,7 @@ private:
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
     static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
     static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+    // Detekce kolizí
+    bool checkCollision(const glm::vec3& position, float radius = 0.3f);
 };
